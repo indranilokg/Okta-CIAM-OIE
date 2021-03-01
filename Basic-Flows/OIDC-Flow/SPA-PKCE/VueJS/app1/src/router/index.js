ab8f4102e89305ae8ebc21ccfeb48afc2b97d990
indranilokg <58342808+indranilokg@users.noreply.github.com>
2021-03-01 15:35:11 -0600

    reorg

---
+++ Basic-Flows/OIDC-Flow/SPA-PKCE/VueJS/app1/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Profile from "../views/Profile.vue";
+import Auth from '@okta/okta-vue'
+
+Vue.use(Auth, {
+  issuer: 'https://acmecorp.twisec.com/oauth2/ausx90vikfMjKld000h7',
+  clientId: '0oax7t53d5ZO1TGvy0h7',
+  redirectUri: 'http://localhost:8080/login/callback',
+  scopes: ['openid', 'profile', 'email'],
+  pkce: true
+})
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  { 
+    path: '/login/callback', 
+    component: Auth.handleCallback() 
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+    meta: {
+      requiresAuth: true
+    }
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+
+export default router;
